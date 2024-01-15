e281c1a0d7b5fbfe39783664aadcf9a3d9a786f0
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2024-01-15 21:52:22 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# 汇总常用插件
+
+function merge_package(){
+# 参数1是分支名,参数2是库地址。所有文件下载到指定路径。
+# 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+trap 'rm -rf "$tmpdir"' EXIT
+branch="$1" curl="$2" target_dir="$3" && shift 3
+rootdir="$PWD"
+localdir="$target_dir"
+[ -d "$localdir" ] || mkdir -p "$localdir"
+tmpdir="$(mktemp -d)" || exit 1
+git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+cd "$tmpdir"
+git sparse-checkout init --cone
+git sparse-checkout set "$@"
+mv -f "$3" "$rootdir"/"$localdir" && cd "$rootdir"
+}
+
+merge_package main https://github.com/kenzok8/small-package package luci-app-adguardhome
+# bypass
+# merge_package master https://github.com/kiddin9/openwrt-packages package luci-app-bypass lua-neturl redsocks2
+# v2raya
+# merge_package master https://github.com/v2rayA/v2raya-openwrt package luci-app-v2raya v2raya 
+
+
+# echo 'src-git alist https://github.com/sbwml/luci-app-alist.git' >>feeds.conf.default
+echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic.git' >>feeds.conf.default
+echo 'src-git lucky https://github.com/sirpdboy/luci-app-lucky.git' >>feeds.conf.default
+# echo 'src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git' >>feeds.conf.default
+git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
+git clone --depth 1 https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
+# git clone -b js --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+
+
+# Add a feed source
+# 添加额外软件包
+# git clone --depth 1  https://github.com/ilxp/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+echo 'src-git cdnspeedtest https://github.com/immortalwrt-collections/openwrt-cdnspeedtest.git' >>feeds.conf.default
+echo 'src-git cloudflarespeedtest https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest.git' >>feeds.conf.default
+
+# 仿istore
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+
+
+# 科学上网插件
+
+# drop mosdns and v2ray-geodata packages that come with the source
+# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+# find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git ssrplus https://github.com/fw876/helloworld.git;master" >>feeds.conf.default
+
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+# 主题
+git clone https://github.com/thinktip/luci-theme-neobird.git feeds/luci/themes/luci-theme-neobird
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git feeds/luci/themes/luci-theme-opentomcat
+git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git feeds/luci/themes/luci-theme-kucat
+git clone https://github.com/derisamedia/luci-theme-alpha.git feeds/luci/themes/luci-theme-alpha
+
+# echo '### Argon Theme Config ###'
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b master  https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config # if have
+git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+
+./scripts/feeds update -a
+./scripts/feeds install -a
---
+++ diy-part2.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# 1.修改默认ip
+sed -i 's/192.168.1.1/192.168.2.3/g' package/base-files/files/bin/config_generate
+
+# 2.修改主机名
+sed -i 's/OpenWrt/Phicomm_N1/g' package/base-files/files/bin/config_generate
+
+# 4.修改版本号
+# sed -i "s/OpenWrt /0012h build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# 5.修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 或
+# default_theme='opentomcat'
+# sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+
+# 6.设置ttyd免登录
+# sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd
+
+# 7.修正连接数
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+
+# 修改kucat标语，来自于sirpdboy
+sed -i 's/做事先做人，尊重他人劳动成果，是为人的基本准则！/Happy Every Day from sirpdboy,Best Regards!/g' feeds/luci/themes/luci-theme-kucat/htdocs/luci-static/kucat/img/logoword
+# feeds/luci/themes/luci-theme-kucat/htdocs/luci-static/kucat/img/logourl
+
+# 替换终端为bash
+# sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+
+# 删除默认密码
+# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
+# 禁用wifi
+# sed -i 's/disabled=0/disabled=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改wifi名字
+# sed -i 's/ssid=OpenWrt/ssid=N1OpenWrt' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 修改本地时间格式
+# sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+# echo '修改时区'
+# sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+# 修改版本为编译日期
+# date_version=$(date +"%y.%m.%d")
+# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# sed -i "s/${orig_version}/R${date_version} by Huoleifeng/g" package/lean/default-settings/files/zzz-default-settings
+
+# 修改晶晨宝盒默认配置
+# 1.设置OpenWrt 文件的下载仓库
+sed -i "s|amlogic_firmware_repo.*|amlogic_firmware_repo 'https://github.com/xuexijiaoben/My_immortalwrt'|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
+
+# 2.设置 Releases 里 Tags 的关键字
+sed -i "s|ARMv8|armvirt|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
+
+# 3.设置 Releases 里 OpenWrt 文件的后缀
+# sed -i "s|.img.gz|.OPENWRT_SUFFIX|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
+
+# 4.设置 OpenWrt 内核的下载路径
+sed -i "s|amlogic_kernel_path.*|amlogic_kernel_path 'https://github.com/breakings/OpenWrt'|g" feeds/amlogic/luci-app-amlogic/root/etc/config/amlogic
+
+
+# Add autocore support for armvirt
+# sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+sed -i 's/DEPENDS:=@(.*/DEPENDS:=@(TARGET_bcm27xx||TARGET_bcm53xx||TARGET_ipq40xx||TARGET_ipq806x||TARGET_ipq807x||TARGET_mvebu||TARGET_rockchip||TARGET_armvirt) \\/g' package/lean/autocore/Makefile
+
+# 编译 po2lmo (如果有po2lmo可跳过)
+# pushd feeds/openclash/luci-app-openclash/tools/po2lmo
+# make && sudo make install
+# popd
