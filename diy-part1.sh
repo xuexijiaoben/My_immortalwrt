d49f17d211959befdcff2827a080e61669ab78d9
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2024-01-15 22:17:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,19 +13,22 @@
 # 汇总常用插件
 
 function merge_package(){
-# 参数1是分支名,参数2是库地址。所有文件下载到指定路径。
-# 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
-trap 'rm -rf "$tmpdir"' EXIT
-branch="$1" curl="$2" target_dir="$3" && shift 3
-rootdir="$PWD"
-localdir="$target_dir"
-[ -d "$localdir" ] || mkdir -p "$localdir"
-tmpdir="$(mktemp -d)" || exit 1
-git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
-cd "$tmpdir"
-git sparse-checkout init --cone
-git sparse-checkout set "$@"
-mv -f "$3" "$rootdir"/"$localdir" && cd "$rootdir"
+    # 参数1是分支名,参数2是库地址。所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
 }
 
 merge_package main https://github.com/kenzok8/small-package package luci-app-adguardhome
