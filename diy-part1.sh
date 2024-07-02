0b5a7a083186c9e48ebc555ee62b9f56bb6ad434
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2024-07-02 14:46:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -74,9 +74,6 @@ echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 # find ./ | grep Makefile | grep mosdns | xargs rm -f
 
-merge_package dev https://github.com/vernesong/OpenClash package/app luci-app-openclash
-# echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default
-
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
@@ -90,6 +87,18 @@ sed -i 's|/usr/bin/gecoosac|/etc/gecoosac/ac_linux_arm64|g' package/app/luci-app
 # sed -i 's|/usr/bin/gecoosac|/etc/gecoosac/ac_linux_arm64|g' package/app/luci-app-gecoosac/root/etc/init.d/gecoosac
 # sed -i 's|/usr/bin/gecoosac|/etc/gecoosac/ac_linux_arm64|g' package/app/luci-app-gecoosac/luasrc/model/cbi/gecoosac.lua
 chmod 755 package/app/luci-app-gecoosac/root/etc/init.d/gecoosac
+
+# 开发版openclash
+merge_package dev https://github.com/vernesong/OpenClash package/app luci-app-openclash
+# echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >>feeds.conf.default
+# 下载openclash内核
+mkdir -p package/app/luci-app-openclash/root/etc/openclash/core/
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz | tar xOvz > package/app/luci-app-openclash/root/etc/openclash/core/clash
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz | gunzip -c > package/app/luci-app-openclash/root/etc/openclash/core/clash_tun
+wget -qO- https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz | tar xOvz > package/app/luci-app-openclash/root/etc/openclash/core/clash_meta
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > package/app/luci-app-openclash/root/etc/openclash/GeoIP.dat
+wget -qO- https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > package/app/luci-app-openclash/root/etc/openclash/GeoSite.dat
+chmod +x package/app/luci-app-openclash/root/etc/openclash/core/clash*
 
 ./scripts/feeds update -a
 \rm -rf feeds/packages/net/cdnspeedtest
